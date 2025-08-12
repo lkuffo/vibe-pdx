8959261968d49390c17f2d6430440ca5313c96da
lkuffo <lxkr@cwi.nl>
2025-08-12 23:18:53 +0200

    Verify PDX

--- export_results.py
+++ export_results.py
@@ -36,6 +36,7 @@ def compute_metrics(path, data_dir):
     with h5py.File(path, "r+") as hfp:
         for query_params in hfp.keys():
             dataset = hfp[query_params].attrs["dataset"]
+            print('Here')
             if "recalls" not in hfp[query_params]:
                 print('Calculating recalls')
                 hfp[query_params]["recalls"] = get_recall_values(
